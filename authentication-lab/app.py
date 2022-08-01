5008251b3af6ae46543daefcf560b3a7e19cd2b6
noada23-meet <109904933+noada23-meet@users.noreply.github.com>
2022-08-01 13:55:42 +0300

    lab

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -21,8 +21,22 @@ db = firebase.database()
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-
 @app.route('/', methods=['GET', 'POST'])
+def signup():
+    error = ""
+    if request.method == 'POST':
+       email = request.form['email']
+       password = request.form['password']
+       try:
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            return redirect(url_for('signin'))
+       except:
+           error = "Authentication failed"
+    else:
+        return render_template("signup.html")
+
+
+@app.route('/signin', methods=['GET', 'POST'])
 def signin():
     error = ""
     if request.method == 'POST':
@@ -33,22 +47,9 @@ def signin():
             return redirect(url_for('add_tweet'))
         except:
             error = "Authentication failed"
-    return render_template("signin.html")
-    
+    else: 
+        return render_template("signin.html")
 
-
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    error = ""
-    if request.method == 'POST':
-       email = request.form['email']
-       password = request.form['password']
-       try:
-            login_session['user'] = auth.create_user_with_email_and_password(email, password)
-            return redirect(url_for('add_tweet'))
-       except:
-           error = "Authentication failed"
-    return render_template("signup.html")
     
 
 
